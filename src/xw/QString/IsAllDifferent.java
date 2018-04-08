8feeee87f80e38ed50bd484a18fe2db0e1e2c25c
xuwang <xuwang@yingxingroup.net>
2018-04-08 15:20:04 +0800

    Initial commit

---
+++ src/xw/QString/IsAllDifferent.java
@@ -0,0 +1,30 @@
+package xw.QString;
+
+/**
+ * 题目：不使用额外的数据结构，确定字符串中所有的字符是否全都不同
+ */
+public class IsAllDifferent {
+
+    static boolean isAllDifferent(String str){
+
+        if(str.length() > 256){
+            return false;
+        }
+
+        boolean[] char_set = new boolean[256];
+
+        for(char a: str.toCharArray()){
+            int test = a;
+            if(char_set[test]){
+                return false;
+            }
+            char_set[test] = true;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        String str = "acde123456";
+        System.out.println("str中所有的字符是全不同的吗  " + isAllDifferent(str));
+    }
+}
---
+++ src/xw/test.java
@@ -0,0 +1,7 @@
+package xw;
+
+/**
+ * Created by Administrator on 2018/4/8.
+ */
+public class test {
+}
