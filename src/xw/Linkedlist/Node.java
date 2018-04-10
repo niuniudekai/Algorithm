6ed62ae7bbe343884c9638102852e02124d81708
xuwang <xuwang@yingxingroup.net>
2018-04-10 09:03:59 +0800

    2018/4/10 9.03

---
+++ src/xw/Linkedlist/Node.java
@@ -0,0 +1,41 @@
+package xw.Linkedlist;
+
+/**
+ * 一个基本的单向链表
+ */
+public class Node {
+    Node next = null;
+    int data;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    /**增加一个结点在尾部*/
+    void add(int d){
+        Node end = new Node(d);
+        Node n = this;
+        while (n.next != null){
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    /**删除一个结点*/
+    Node deleteNode(Node head,int d){
+        Node start = head;
+
+        if(start.data == d){
+            return head.next;
+        }
+
+        while (start.next != null){
+            if(start.next.data == d){
+                start.next = start.next.next;
+                return head;
+            }
+            start = start.next;
+        }
+        return head;
+    }
+}
