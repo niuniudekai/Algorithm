78ba352f1e5e906b55341db381b3c1617654b8a1
xuwang <xuwang@yingxingroup.net>
2018-04-11 13:53:57 +0800

    2018/4/11 13.53

--- src/xw/Sort/quickSort.java
+++ src/xw/Sort/quickSort.java
@@ -1,7 +1,13 @@
 package xw.Sort;
 
+import com.sun.xml.internal.bind.v2.model.annotation.Quick;
+
+import java.util.Arrays;
+import java.util.Stack;
+
 public class quickSort {
 
+    /**递归*/
     static void QSort(int[] arr,int low ,int high){
         if(low >= high){
             return;
@@ -24,12 +30,47 @@ public class quickSort {
         QSort(arr,i + 1, high);
     }
 
+    /**非递归*/
+    static void QSort2(int[] arr,int low, int high){
+        Stack<Integer> stack = new Stack<>();
+        if(low < high){
+            stack.push(high);
+            stack.push(low);
+            while (!stack.isEmpty()){
+                int l = stack.pop();
+                int h = stack.pop();
+                int dex = Quick(arr,l,h);
+                if (l < dex - 1){
+                    stack.push(dex - 1);
+                    stack.push(l);
+                }
+                if (h > dex + 1){
+                    stack.push(h);
+                    stack.push(dex + 1);
+                }
+            }
+        }
+    }
+
+    static int Quick(int[] arr, int low, int high){
+        int key = arr[low];
+        while (low < high){
+            while (low < high && arr[high] >= key){
+                high--;
+            }
+            arr[low] = arr[high];
+            while (low < high && arr[low] <= key){
+                low++;
+            }
+            arr[high] = arr[low];
+        }
+        arr[low] = key;
+        return low;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,425,5,3423,45,5,23};
-        QSort(arr,0,arr.length - 1);
-        System.out.println();
-        for(int a:arr){
-            System.out.print(a + " ");
-        }
+        QSort2(arr,0,arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/xw/test.java
+++ src/xw/test.java
@@ -4,4 +4,11 @@ package xw;
  * Created by Administrator on 2018/4/8.
  */
 public class test {
+
+    public static void main(String[] args){
+         String str0="0123456789";
+        String str1="0123456789";
+        String str2=str1.substring(5);
+        System.out.println(str2);
+    }
 }
