ceac2873092594d67842cd83ae9b0aeab26064c1
xuwang <xuwang@yingxingroup.net>
2018-04-12 14:12:36 +0800

    2018/4/12 10.39

--- src/xw/test.java
+++ src/xw/test.java
@@ -1,14 +1,51 @@
 package xw;
 
+import java.io.*;
+
 /**
  * Created by Administrator on 2018/4/8.
  */
 public class test {
 
-    public static void main(String[] args){
-         String str0="0123456789";
-        String str1="0123456789";
-        String str2=str1.substring(5);
-        System.out.println(str2);
+
+    public static void main(String args[]){
+        String fileName = "d:\\test.txt";
+        File file = new File(fileName);
+        Long filelength = file.length();
+        System.out.println(filelength);
+        byte[] filecontent = new byte[filelength.intValue()];
+        try {
+//            FileInputStream in = new FileInputStream(file);
+//            in.read(filecontent);
+//            in.close();
+            BufferedReader in = new BufferedReader (new FileReader(fileName));
+            String s;
+            StringBuilder content = new StringBuilder();
+            while ((s = in.readLine()) != null){
+                content.append(s);
+            }
+
+
+
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+            out.write("ssss\nsss");
+            out.close();
+            System.out.println(content);
+
+            while ((s = in.readLine()) != null){
+                content.append(s);
+            }
+
+            in.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
     }
+
 }
