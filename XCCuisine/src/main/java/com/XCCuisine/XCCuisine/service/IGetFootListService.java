9945a184a2b187bd58db1b5202c50eaf4572da24
xbtshady <xx943217258>
2019-03-18 20:48:39 +0800

    delete

--- XCCuisine/src/main/java/com/XCCuisine/XCCuisine/service/IGetFootListService.java
+++
@@ -1,9 +0,0 @@
-package com.XCCuisine.XCCuisine.service;
-
-import com.XCCuisine.XCCuisine.po.Food;
-
-import java.util.List;
-
-public interface IGetFootListService {
-    List<Food> getFootListService();
-}
